e2ec0efb206d42ad5d85662b2839dcd62d2a37dd
github username <github email address>
2016-09-29 14:25:04 -0400

    all tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word_to_check)
+    @word = word_to_check
+  end
+
+  def match(array_of_possible_answers)
+    array_of_possible_answers.map do |word|
+      word if word.split("").sort == @word.split("").sort
+    end.compact
+  end
+
+end
